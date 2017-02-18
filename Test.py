85163f3716cd3c5daf841169476bec3ddd326ff9
darius <darius@dari.us>
2017-02-18 16:15:27 -0500

    I added a parsing plugin called beautiful soup to remove all the html garbage. I added pickle which is a dictionary object. I have the beginning of a corpus that i am making using wget on terminal. the corpus will become very very big when im done taking the lyrics to every rap song every recorded.

--- Test.py
+++ Test.py
@@ -1,2 +1,7 @@
-print("hello")
+import pickle
+from bs4 import BeautifulSoup
+import csv
 
+soup = BeautifulSoup(open("/Users/darius/Documents/ComSci2/project4/lyricsmode/backseat.ken.txt"), "html.parser")
+
+print(soup.get_text())
