df52983b0f60840e0eccddc3521ef04b0e9a94d5
darius <darius@dari.us>
2017-02-19 11:01:01 -0500

    fixed big unicode encoding errors that caused 13 files to not be read. Added $ character and # character at beginning and end of file o as to teach the computer when that certain words appear more often at beginning and end of line. currently file opens all 113 files and turns splits them into line arrays.

--- Test.py
+++ Test.py
@@ -4,27 +4,37 @@ import csv
 import re
 import os
 import sys
+sys.getdefaultencoding()
+badcount = 0
 path = '/Users/darius/Documents/ComSci2/project4/lyricsmode'
 for filename in os.listdir(path):
+	#filename = filename.decode('utf8')
 	myfile = path+"/"+filename
 	#print(myfile)
 	pretext = ''
 	t= ''
+	t2= u''	
 	try:
-		soup = BeautifulSoup(open(myfile), "html.parser")
+		t2 = open(myfile, encoding="utf-8").read()
+	except:
+		t2 = open(myfile, encoding="latin-1").read()
+		print("fallback to latin 1:", sys.exc_info()[1])
+		#e = sys.exc_info()[0]
+		print("latin file: \n"+ myfile)
+	try:
+		soup = BeautifulSoup(t2, "html.parser")
 		#soup = BeautifulSoup.BeautifulSoup(content.decode('utf-8','ignore'))
 		pretext = soup.find_all('pre')
-	except:
-		#print("Unexpected error:", sys.exc_info()[0])
-		#e = sys.exc_info()[0]
-		print("badfile: \n"+ myfile)
+	except: 
+		badcount+=1
+		print("Unexpected error from soup:", sys.exc_info()[1])
 	
 	if len(pretext) > 0:
 		for t in pretext:
 			t = t.get_text()
 	else:
 		try:
-			rawfile = open(myfile)
+			rawfile = open(myfile, encoding='latin1')
 			t = rawfile.read()
 		except:
 			print("badfile2 :"+ myfile)
@@ -38,3 +48,4 @@ for filename in os.listdir(path):
 		if re.match('\w+',line):
 			newline = '$ ' + line + ' #'
 			#print(newline)
+print(badcount)
