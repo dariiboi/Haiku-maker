74b7e8758a5834c6a605fd47a745d5eced3c4aa1
darius <darius@dari.us>
2017-02-19 21:48:47 -0500

    began counting words. annotated a little bit and created dictionary in a dictionary. number counts do not equal probabilites yet and it only counts 1 word back.

--- Test.py
+++ Test.py
@@ -1,3 +1,4 @@
+import string 
 import pickle
 from bs4 import BeautifulSoup
 import csv
@@ -7,6 +8,24 @@ import sys
 sys.getdefaultencoding()
 badcount = 0
 path = '/Users/darius/Documents/ComSci2/project4/lyricsmode'
+dict1 = {}
+dict2 = {}
+#count the words
+def count(line):
+	global dict1
+	global dict2
+	words = line.split(' ')
+	for i in range(2,len(words)):
+		if words[i] in dict1:
+			if words[i-1] in dict1[words[i]]:
+				dict1[words[i]][words[i-1]] += 1
+			else:
+				dict1[words[i]][words[i-1]] = 1
+		else:
+			dict1[words[i]]={}
+
+
+
 for filename in os.listdir(path):
 	#filename = filename.decode('utf8')
 	myfile = path+"/"+filename
@@ -15,11 +34,13 @@ for filename in os.listdir(path):
 	t= ''
 	t2= u''	
 	try:
-		t2 = open(myfile, encoding="utf-8").read()
+		f = open(myfile, 'rb')
+		t2 = f.read().decode('utf8', 'ignore')
+		#t2 = open(myfile, encoding="utf-8").read()
 	except:
-		t2 = open(myfile, encoding="latin-1").read()
+		t2 = open(myfile, encoding="latin-1 ").read()
 		print("fallback to latin 1:", sys.exc_info()[1])
-		#e = sys.exc_info()[0]
+		e = sys.exc_info()[0]
 		print("latin file: \n"+ myfile)
 	try:
 		soup = BeautifulSoup(t2, "html.parser")
@@ -27,6 +48,7 @@ for filename in os.listdir(path):
 		pretext = soup.find_all('pre')
 	except: 
 		badcount+=1
+		#if all other checks fail, go here
 		print("Unexpected error from soup:", sys.exc_info()[1])
 	
 	if len(pretext) > 0:
@@ -41,11 +63,15 @@ for filename in os.listdir(path):
 	t = t.lower()
 	t = re.sub("[\(\[].*?[\)\]]", "", t)
 	t = re.sub("[^a-z0-9' \n]*", "", t)
+
 	#print(t)
 	lines = t.split('\n')
 	lines = lines[6:]
 	for line in lines:
+		line = ' '.join(line.split())
 		if re.match('\w+',line):
 			newline = '$ ' + line + ' #'
-			#print(newline)
-print(badcount)
+			count(newline)
+print(dict1['#'])
+
+
